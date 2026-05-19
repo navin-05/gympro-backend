9738027b0e50417a7ff186313d44b99d5c1d1a75
navin-05 <navin.j2022cse@sece.ac.in>
2026-05-19 18:45:51 +0530

    essential changes

--- services/whatsappClient.js
+++ services/whatsappClient.js
@@ -1,9 +1,5 @@
-const path = require('path');
 const QRCode = require('qrcode');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-
-const AUTH_PATH = process.env.WWEBJS_AUTH_PATH
-  || path.join(__dirname, '..', '.wwebjs_auth');
 
 let client = null;
 let ready = false;
@@ -12,10 +8,21 @@ let initPromise = null;
 function getClient() {
   if (!client) {
     client = new Client({
-      authStrategy: new LocalAuth({ dataPath: AUTH_PATH }),
+      authStrategy: new LocalAuth({
+        dataPath: './.wwebjs_auth',
+      }),
       puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process',
+          '--disable-gpu',
+        ],
       },
     });
 
